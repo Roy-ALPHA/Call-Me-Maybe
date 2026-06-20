a1d60a65550b05116bf475b872efce67ebdd983b
Roy-ALPHA <boudaliyounss@gmail.com>
2026-06-21 00:02:40 +0100

    save

--- src/core/engine.py
+++ src/core/engine.py
@@ -8,6 +8,7 @@ import time
 from .validators import *
 from .Trie import TrieNode, Trie
 from multiprocessing import Pool, cpu_count
+from functools import cached_property
 
 
 class FunctionCallingEngine(BaseModel):
@@ -17,55 +18,71 @@ class FunctionCallingEngine(BaseModel):
     model: Small_LLM_Model = Small_LLM_Model()
     def_funcs: list[dict]
     inpt_prompts: list[dict]
-    funcs_test: list = []
+    base_prompt: str = ""
 
     @staticmethod
     def mask_low_scores(allowed_tokens, logits):
         return {token: logits[token] for token in allowed_tokens}
 
-    def extract_func(self, cur_promt):
-        # print(cur_promt)
-        # exmple = json.dumps(self.def_funcs[0].__repr__())
-        # print(exmple)
-        # prompt = "prompt: " + f"{cur_promt}" + "\nfrom this prompt, select the best matching dict from this list of dicts:\n" + self.def_funcs.__repr__() + "\nyour output must be like this:\n" + exmple
-        with open("src/core/prompt.txt") as f:
-            prompt = f.read() + "\nUser Request:\n" + cur_promt + "\nAvailable Functions:\n" + json.dumps(self.def_funcs) + "\nReturn the best matching function dictionary (only json output)."
-        # print(prompt)
+    @cached_property
+    def _build_trie(self):
         trie = Trie()
 
         for func in self.def_funcs:
-            # print(func.__repr__())
-            trie.insert(self.model.encode(" " + json.dumps(func)).numpy().ravel().tolist()[:-1] + [95642])
-        # print(self.model.encode(" " + json.dumps(self.def_funcs[-1])).numpy().ravel().tolist()[-1], self.model.encode("\n}}"))
-        # [74491]
-        generated_tokens = []
-        correct_func = ""
+            trie.insert(
+                self.model.encode(" " + func["name"])
+                .numpy()
+                .ravel()
+                .tolist()
+            )
+
+        with open("src/core/prompt.txt") as f:
+            self.base_prompt = f.read()
+
+        return trie
+
+    def _build_prompt(self, cur_prompt):
+
+        prompt = (
+            self.base_prompt +
+            "\nUser Request:\n" + cur_prompt +
+            "\nAvailable Functions:\n" +
+            json.dumps(self.def_funcs) +
+            "\nDetermine the best matching function name.\n"
+            "The answer is: "
+        )
+
+        return prompt
+
+
+    def extract_func(self, cur_prompt):
+        
+        trie, prompt = (self._build_trie, self._build_prompt(cur_prompt))
+
+        func = []
 
         cur_node = trie.root
         while not cur_node.is_end:
             prompt_tokens = self.model.encode(prompt).numpy().ravel().tolist()
 
-            logits = self.model.get_logits_from_input_ids(prompt_tokens + generated_tokens)
-
-            top_k = np.argmax(np.array(logits))
+            logits = np.array(self.model.get_logits_from_input_ids(prompt_tokens + func))
 
             allowed_tokens = trie.get_allowed_next_tokens(cur_node)
 
-            generated_tokens.append(top_k)
-            # print(self.model.decode(top_k), top_k)
-            if top_k in allowed_tokens:
-                valid_tokens = FunctionCallingEngine.mask_low_scores(allowed_tokens, logits)
+            masked_logits = np.full_like(logits, -np.inf)
 
-                best_token = max(valid_tokens, key=valid_tokens.get)
+            masked_logits[allowed_tokens] = logits[allowed_tokens]
 
-                correct_func += self.model.decode(best_token)
-                # print(correct_func)
-                cur_node = trie.get_node(best_token, cur_node)
+            best_token = np.argmax(masked_logits)
 
-        return json.loads(json.dumps(correct_func))
+            func.append(best_token)
+
+            cur_node = trie.get_node(best_token, cur_node)
+
+        return {"prompt": cur_prompt, "func_name": self.model.decode(func)}
     
     def test(self):
-        with Pool(processes=cpu_count()) as pool:
-            res = pool.map(self.extract_func, [prompt["prompt"] for prompt in self.inpt_prompts])
-        
-        print(res)
+        start = time.perf_counter()
+        for prompt in self.inpt_prompts:
+            print(self.extract_func(prompt["prompt"]))
+        print(time.perf_counter() - start)
